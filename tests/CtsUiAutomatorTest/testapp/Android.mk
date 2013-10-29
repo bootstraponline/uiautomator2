a006b2db00f06a372cb141405f9b47484ea368b6
Allen Hair <allenhair@google.com>
2013-10-29 11:28:36 -0700

    Fixed testapp module tag to match CtsUiAutomatorTest.
    Change-Id: I0284b5cf8093f392afded335f20648dba3ff2aa6

--- tests/CtsUiAutomatorTest/testapp/Android.mk
+++ tests/CtsUiAutomatorTest/testapp/Android.mk
@@ -16,7 +16,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := tests
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
